ef9d5f92fac9014d3b57d104116c8f0508efb42e
Moinak_Mnx <111623431+Cyberninja30@users.noreply.github.com>
2025-06-22 15:57:06 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -323,8 +323,8 @@ function App() {
 
         {/* Certifications Section */}
         
-        <section id="certifications">
-          <h2 className="section-title glitch-text" data-text="Certifications & Achievements">Certifications & Achievements</h2>
+        <section id="Achievements">
+          <h2 className="section-title glitch-text" data-text="Certifications &Achievements">Achievements</h2>
           <div className="section-underline">==============================</div>
           
           <div className="timeline-section">
